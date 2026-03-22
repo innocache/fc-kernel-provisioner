818a2acad99fd8bacc3d413521d06015443acc0f
innocache <wang2xu4@gmail.com>
2026-03-21 20:06:13 -0700

    feat: add guest init script and rootfs build script

---
+++ guest/init.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Minimal /init for Firecracker microVM.
+# Static IP is configured via kernel boot args (ip= parameter).
+mount -t proc proc /proc
+mount -t sysfs sys /sys
+mount -t devtmpfs dev /dev
+mkdir -p /run /tmp
+
+# Bring up loopback
+ip link set lo up
+
+# eth0 is configured by kernel boot args: ip=<ip>::<gw>:<mask>::eth0:off
+# Just need to bring the link up
+ip link set eth0 up
+
+# Start guest agent as PID 1's child
+exec python3 /usr/local/bin/fc-guest-agent
